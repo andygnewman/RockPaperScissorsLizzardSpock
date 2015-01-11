d7f569bfdd17e39b89d16753ff6fddfb986cf97d
Andy <andygnewman@gmail.com>
2015-01-11 18:58:28 +0000

    bonus 2 achieved added lizzard and spock as options

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ class Game
   attr_reader :players
 
   def initialize
-    @rules = {rock: :scissors, paper: :rock, scissors: :paper}
+    @rules = {rock: [:scissors, :lizzard], paper: [:rock, :spock], scissors: [:paper, :lizzard], lizzard: [:spock, :paper], spock: [:scissors, :rock]}
     @players = [@player1 = {name: "Player 1", score: 0, choice: nil}, @player2 = {name: "Computer", score:0, choice: nil }]
   end
 
@@ -38,7 +38,7 @@ class Game
     computer_choice if player2[:name] == "Computer"
     if player1[:choice] == player2[:choice]
       "It's a draw! You both chose #{choice_display(player1[:choice])}"
-    elsif player2[:choice] == @rules[player1[:choice]]
+    elsif @rules[player1[:choice]].include?(player2[:choice])
       player1[:score] += 1
       "#{player1[:name]} wins; #{choice_display(player1[:choice])} beats #{choice_display(player2[:choice])}!"
     else
